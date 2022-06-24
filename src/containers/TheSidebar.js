8fe177eeef2e11da8399641ae68193727998ab97
Stanislas-Web <stanislasmakengo1@gmail.com>
2022-06-24 14:30:43 +0100

    update border

--- src/containers/TheSidebar.js
+++ src/containers/TheSidebar.js
@@ -27,8 +27,8 @@ const TheSidebar = () => {
       show={show}
       onShowChange={(val) => dispatch({ type: "set", sidebarShow: val })}
     >
-      <CSidebarBrand className="d-md-down-none" style={{textDecoration:"none", backgroundColor:"white", borderRight:"1px solid grey", borderBottom:"1px solid grey"}} to="/">
-        <div className="c-sidebar-brand-full" style={{fontWeight:"300", fontSize:"13px", color:"black"}}><img src={logo} width="50px" height="30px"/> Akilimali </div>
+      <CSidebarBrand className="d-md-down-none" style={{textDecoration:"none", backgroundColor:"white", borderRight:"1px solid #C4C4C4", borderBottom:"1px solid grey"}} to="/">
+        <div className="" style={{fontWeight:"300",display:"flex",fontSize:"13px", color:"black"}}><img src={logo} width="50px" height="30px"/> Akilimali </div>
       </CSidebarBrand>
 
       <CSidebarNav>
